28a17f37060681a08be212a55da3bf6eace41fd2
SulRash <sultan.m.rashed@gmail.com>
2024-02-02 16:58:45 +0000

    Adjusted test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 accelerate launch pretrain.py \
-    --data_path "test.jsonl" \
+    --data_path "." \
     --config_path "TinyLlama/TinyLlama-1.1B-Chat-v1.0" \
     --tokenizer_path "TinyLlama/TinyLlama-1.1B-Chat-v1.0" \
     --save_dir "runs/"
